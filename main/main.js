84ea702b33b43040e313590ac148b7720cba8474
seven <seven@sevendeMBP.lan>
2019-05-22 11:00:34 +0800

    taxi fee

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var sum = 0;
+    var dis = inputs["distance"];
+    if (dis <= 2) {
+        sum += 6;
+    } else if (dis <= 8) {
+        sum += 0.8 * (dis - 2) + 6;
+    } else {
+        sum += 1.2 * (dis - 8) + 6 + 6 * 0.8;
+    }
+
+    sum += 0.25 * inputs["parkTime"];
+    sum = Math.round(sum);
+
+    return sum;
 };
